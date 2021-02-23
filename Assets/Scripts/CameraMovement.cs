a15c7bdaeec1e53e487b93ab4ec8a69ee32acbf5
tupacsharp@hotmail.com <tupacsharp@hotmail.com>
2021-02-23 16:34:08 +0200

    update cameraMovement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -18,10 +18,10 @@ public class CameraMovement : MonoBehaviour
     {
         float x = Mathf.Max(Mathf.Min(target.position.x,maxPosition.x),minPosition.x);
         float y = Mathf.Max(Mathf.Min(target.position.y, maxPosition.y), minPosition.y);
-        x = target.position.x;
-        y = target.position.y;
+       // x = target.position.x;
+        //y = target.position.y;
         Vector3 v = new Vector3(x, y, transform.position.z);
-        // Vector3 v = new Vector3(target.position.x, target.position.y, transform.position.z);
+       // Vector3 v = new Vector3(target.position.x, target.position.y, transform.position.z);
 
         if (transform.position != target.position)
         {
