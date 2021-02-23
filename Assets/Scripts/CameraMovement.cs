64dbf316e661c0990baf09e3235a535df21ed23f
tupacsharp@hotmail.com <tupacsharp@hotmail.com>
2021-02-23 16:04:55 +0200

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+    void Start()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        float x = Mathf.Max(Mathf.Min(target.position.x,maxPosition.x),minPosition.x);
+        float y = Mathf.Max(Mathf.Min(target.position.y, maxPosition.y), minPosition.y);
+        x = target.position.x;
+        y = target.position.y;
+        Vector3 v = new Vector3(x, y, transform.position.z);
+        // Vector3 v = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        if (transform.position != target.position)
+        {
+            transform.position = Vector3.Lerp(transform.position,v, smoothing);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    Vector2 change;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector2.zero;
+       
+        change.x=Input.GetAxisRaw("Horizontal");
+        change.y=Input.GetAxisRaw("Vertical");
+       
+
+
+    }
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector2.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+    private void FixedUpdate()
+    {
+        UpdateAnimationAndMove();
+    }
+    public void MoveCharacter()
+    {
+      
+        myRigidbody.MovePosition(myRigidbody.position + change * speed * Time.deltaTime);
+    }
+}
